9f2de40ea7deea10485e844886f0cea7c64b274f
Zeno Dalla Valle <zenodallavalle@gmail.com>
2023-06-29 19:39:26 +0200

    added device field

--- src/api.js
+++ src/api.js
@@ -1,6 +1,15 @@
 import { useCallback } from 'react';
 import { useGoogleReCaptcha } from 'react-google-recaptcha-v3';
 import { v4 as uuidv4 } from 'uuid';
+import { isDesktop, isMobileOnly, isTablet } from 'react-device-detect';
+
+const deviceType = isMobileOnly
+  ? 1 // mobile
+  : isTablet
+  ? 2 // tablet
+  : isDesktop
+  ? 3 // desktop
+  : 4; // other
 
 const getUID = () => {
   const localUID = localStorage.getItem('uid');
@@ -10,37 +19,31 @@ const getUID = () => {
   return newUID;
 };
 
-export const collectStart = async (quizUID, nickname, job, token) => {
-  const url = new URL('start_event/', process.env.REACT_APP_API_URL);
-  try {
-    await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        authorization: process.env.REACT_APP_AUTH_KEY,
-      },
-      body: JSON.stringify({
-        quiz_uid: quizUID,
-        device_uid: getUID(),
-        recaptcha_token: token,
-        nickname,
-        job,
-      }),
-    });
-  } catch (error) {
-    console.error('Error in collectStart: ', error);
-  }
-};
-
 export const useCollectStart = () => {
   const { executeRecaptcha } = useGoogleReCaptcha();
+
   const returnFn = useCallback(
     async (quizUID, nickname, job) => {
       if (!executeRecaptcha) return;
       try {
         const recaptchaToken = await executeRecaptcha('collectStart');
-        collectStart(quizUID, nickname, job, recaptchaToken);
+        const url = new URL('start_event/', process.env.REACT_APP_API_URL);
+        await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            authorization: process.env.REACT_APP_AUTH_KEY,
+          },
+          body: JSON.stringify({
+            device_type: deviceType,
+            quiz_uid: quizUID,
+            device_uid: getUID(),
+            recaptcha_token: recaptchaToken,
+            nickname,
+            job,
+          }),
+        });
       } catch (error) {
         console.error('Error in useCollectStart: ', error);
       }
@@ -50,59 +53,36 @@ export const useCollectStart = () => {
   return { collectStart: returnFn };
 };
 
-const collectAnswer = async (
-  quizUID,
-  nickname,
-  job,
-  qId,
-  answerId,
-  questionText,
-  answerText,
-  recaptchaToken
-) => {
-  const url = new URL('answer_event/', process.env.REACT_APP_API_URL);
-  try {
-    await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        authorization: process.env.REACT_APP_AUTH_KEY,
-      },
-      body: JSON.stringify({
-        quiz_uid: quizUID,
-        device_uid: getUID(),
-        recaptcha_token: recaptchaToken,
-        nickname,
-        job,
-        question_id: qId,
-        answer_id: answerId,
-        question_text: questionText,
-        answer_text: answerText,
-      }),
-    });
-  } catch (error) {
-    console.error('Error in collectAnswer: ', error);
-  }
-};
-
 export const useCollectAnswer = () => {
   const { executeRecaptcha } = useGoogleReCaptcha();
+
   const returnFn = useCallback(
     async (quizUID, nickname, job, qId, answerId, questionText, answerText) => {
       if (!executeRecaptcha) return;
       try {
         const recaptchaToken = await executeRecaptcha('collectAnswer');
-        collectAnswer(
-          quizUID,
-          nickname,
-          job,
-          qId,
-          answerId,
-          questionText,
-          answerText,
-          recaptchaToken
-        );
+        const url = new URL('answer_event/', process.env.REACT_APP_API_URL);
+
+        await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            authorization: process.env.REACT_APP_AUTH_KEY,
+          },
+          body: JSON.stringify({
+            device_type: deviceType,
+            quiz_uid: quizUID,
+            device_uid: getUID(),
+            recaptcha_token: recaptchaToken,
+            nickname,
+            job,
+            question_id: qId,
+            answer_id: answerId,
+            question_text: questionText,
+            answer_text: answerText,
+          }),
+        });
       } catch (error) {
         console.error('Error in useCollectAnswer: ', error);
       }
@@ -112,38 +92,33 @@ export const useCollectAnswer = () => {
   return { collectAnswer: returnFn };
 };
 
-const collectResult = async (quizUID, nickname, job, marks, recaptchaToken) => {
-  const url = new URL('result_event/', process.env.REACT_APP_API_URL);
-  try {
-    await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        authorization: process.env.REACT_APP_AUTH_KEY,
-      },
-      body: JSON.stringify({
-        quiz_uid: quizUID,
-        device_uid: getUID(),
-        recaptcha_token: recaptchaToken,
-        nickname,
-        job,
-        marks,
-      }),
-    });
-  } catch (error) {
-    console.error('Error in collectAnswer: ', error);
-  }
-};
-
 export const useCollectResult = () => {
   const { executeRecaptcha } = useGoogleReCaptcha();
+
   const returnFn = useCallback(
     async (quizUID, nickname, job, marks) => {
       if (!executeRecaptcha) return;
       try {
         const recaptchaToken = await executeRecaptcha('collectResult');
-        collectResult(quizUID, nickname, job, marks, recaptchaToken);
+        const url = new URL('result_event/', process.env.REACT_APP_API_URL);
+
+        await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            authorization: process.env.REACT_APP_AUTH_KEY,
+          },
+          body: JSON.stringify({
+            device_type: deviceType,
+            quiz_uid: quizUID,
+            device_uid: getUID(),
+            recaptcha_token: recaptchaToken,
+            nickname,
+            job,
+            marks,
+          }),
+        });
       } catch (error) {
         console.error('Error in useCollectResult: ', error);
       }
