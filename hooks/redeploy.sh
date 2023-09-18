32f31ca0b68216c2e71313e8e8ac725582b15fe6
Zeno Dalla Valle <zenodallavalle@gmail.com>
2023-09-18 09:52:10 +0200

    updated hooks

--- hooks/redeploy.sh
+++ hooks/redeploy.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-cd /var/www/hand-hygiene-contest-quiz-frontend
 git pull -f origin main
 yarn
 yarn build
 rm -rf build
 cp ./src/sourceQuestions.js ./building/sourceQuestions.js
-mv ./building ./build
-sudo service apache2 restart
+echo "Copy or move ./building to your server folder and restart apache2"
